ac041718079f5c87ce2fbb4826845f259554110d
Commander <root@cia.com>
2025-04-30 15:54:44 +0300

    working on it

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ def main():
 def Testing():
     # print(str(RawJson.read()))
     item_dict = json.loads(str(RawJson.read()))
-    print(item_dict)
-    print (len(item_dict['text'][0]['sectionNames']))
+    # print(item_dict)
+    print(item_dict['text'])
+    # print (len(item_dict['text'][0]['sectionNames']))
 
 
 
