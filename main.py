85e14e09b46045a84b4fd6f2cb25ab6421371230
Commander <root@cia.com>
2025-04-30 15:56:22 +0300

    get the nth item

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def Testing():
     # print(str(RawJson.read()))
     item_dict = json.loads(str(RawJson.read()))
     # print(item_dict)
-    print(item_dict['text'])
+    print(item_dict['text'][90])
     # print (len(item_dict['text'][0]['sectionNames']))
 
 
