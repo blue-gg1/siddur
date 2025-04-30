b62da75f28e53e7761a8d5682d9033c632f3cf23
Commander <root@cia.com>
2025-04-30 15:49:29 +0300

    Traceback (most recent call last):   File "/home/fucker/Documents/code/siddur/main.py", line 24, in <module>     Testing()   File "/home/fucker/Documents/code/siddur/main.py", line 14, in Testing     item_dict = json.loads(RawJson)   File "/usr/lib/python3.10/json/__init__.py", line 339, in loads     raise TypeError(f'the JSON object must be str, bytes or bytearray, ' TypeError: the JSON object must be str, bytes or bytearray, not TextIOWrapper (.venv) fucker@pop-os:~/Documents/code/siddur$ ^C

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os, json
 import pandas as pd 
 
+
 def LoadJsonFromFile():
+    global RawJson
     RawJson = open(str(os.getcwd())+"/Text/TanachPublicDomain.json")
-    DataFrame = pd.read_json(RawJson)
-    print(DataFrame)
-
-
-
-
-
-
 
 def main():
     print("starting")
 
+
+def Testing():
+    item_dict = json.loads(RawJson)
+    print (len(item_dict['result'][0]['run']))
+
+
+
+
+
+
 # main()
 LoadJsonFromFile()
+Testing()
